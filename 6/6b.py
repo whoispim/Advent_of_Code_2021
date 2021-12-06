5348694b824e8697eda7eecc8ae3e308b61a8975
steska <tim.steska@ufz.de>
2021-12-06 16:26:22 +0100

    Tag 6b dauert für immer

--- 6/6b.py
+++ 6/6b.py
@@ -7,26 +7,29 @@ This is a temporary script file.
 
 import numpy as np
 import time
-start_time = time.time()
 
-fishies = np.genfromtxt("inputtest", delimiter=",", dtype="int")
+with open("input","r") as f:
+    fishies = [int(fish) for fish in f.readline().split(",")]
 
-for day in range(80):
-    fishies -= 1
-    fishmask = fishies < 0
-    fishies [fishmask] = 6
-    for i in range(np.count_nonzero(fishmask)):
-        fishies = np.append(fishies, 8)
-            
+for day in range(256):
+    start_time = time.time()
+    print(F"Day {day}...")
+    Neos = fishies.count(0)
+    fishies = [6 if fish==0 else fish-1 for fish in fishies]
+    for i in range(Neos):
+        fishies.append(8)
+    print("Done in %s seconds" % (time.time() - start_time))
+    
 print(F"Number of Fishies: {len(fishies)}")
-print(" %s seconds" % (time.time() - start_time))
 
 #testfile: 
 #Number of Fishies: 5934
-#a 0.06827664375305176 seconds
-#b 0.02988719940185547 seconds
+#a  0.06827664375305176 seconds
+#b  0.02988719940185547 seconds
+#bb 0.0050122737884521484 seconds
 
 #real input
 #Number of Fishies: 359344
 #a 48.101457357406616 seconds
 #b 58.29901742935181 seconds
+#bb 0.312028169631958 seconds
