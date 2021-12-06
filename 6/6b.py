604031dcf6c42a865770a2c0723953ea5fb110bb
whoispim <pimbot@bk.ru>
2021-12-06 17:22:53 +0100

    The Solution: Buckets

--- 6/6b.py
+++ 6/6b.py
@@ -5,10 +5,9 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import numpy as np
 import time
 
-with open("input","r") as f:
+with open("inputtest","r") as f:
     fishies = [int(fish) for fish in f.readline().split(",")]
 
 for day in range(256):
@@ -21,15 +20,3 @@ for day in range(256):
     print("Done in %s seconds" % (time.time() - start_time))
     
 print(F"Number of Fishies: {len(fishies)}")
-
-#testfile: 
-#Number of Fishies: 5934
-#a  0.06827664375305176 seconds
-#b  0.02988719940185547 seconds
-#bb 0.0050122737884521484 seconds
-
-#real input
-#Number of Fishies: 359344
-#a 48.101457357406616 seconds
-#b 58.29901742935181 seconds
-#bb 0.312028169631958 seconds
---
+++ 6/6bb.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import time
+start_time = time.time()
+
+with open("input","r") as f:
+    fishies = [int(fish) for fish in f.readline().split(",")]
+
+fishbucket=[]
+for a in range(9):
+    fishbucket.append(fishies.count(a))
+
+for day in range(256):
+    fishbucket = fishbucket[1:7]\
+        + [fishbucket[0]+fishbucket[7]]\
+        + [fishbucket[8]]\
+        + [fishbucket[0]]
+    
+print("Done in %s seconds" % (time.time() - start_time))
+print(F"Number of Fishies: {sum(fishbucket)}")
