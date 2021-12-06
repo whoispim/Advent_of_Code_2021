53b3338d7c9a7e318e15b9d4de0ce1e7c33b46ec
whoispim <pimbot@bk.ru>
2021-12-06 17:29:27 +0100

    Reformatted

--- 5/5b.py
+++ 5/5b.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import numpy as np
 
 xyxy = np.empty((0,4), int)
--- 6/6b.py
+++ 6/6b.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 import time
 
 with open("inputtest","r") as f:
